ae8d21704d9ace4e77399915ca025247cfa3fb8d
Daniel Lee <lee0118@gmail.com>
2020-08-07 12:45:24 -0400

    worked with bash.js and added nodemon

--- bash.js
+++ bash.js
@@ -1,1 +1,16 @@
 /* hi */
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    const pwd = process.cwd();
+
+    if (cmd == 'pwd'){
+        process.stdout.write(pwd + '\nprompt > ');
+    }
+    else{
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt > ');
+    }
+})
